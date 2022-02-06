1d5b807fa461af294386c4ccbb74cd0ad81f84b6
unknown <ctpluong@gmail.com>
2022-02-06 16:32:46 +0700

    init

---
+++ AmoObject.h
@@ -0,0 +1,45 @@
+#ifndef AMO_OBJECT_H_
+#define AMO_OBJECT_H_
+#include "BaseObject.h"
+
+#define WIDTH_LASER 35
+#define HEIGHT_LASER 5
+
+#define WIDTH_SPHERE 30
+#define HEIGHT_SPHERE 30
+
+class AmoObject : public BaseObject
+{
+public:
+    enum AmoType
+    {
+        NONE = 0,
+        LASER = 1,
+        SPHERE = 2
+    };
+
+    AmoObject();
+    ~AmoObject();
+    void HandleInputAction(SDL_Event events);
+    void HandleMove(const int& x_border, const int& y_border);
+
+    int get_type() const { return amo_type_; }
+    void set_type(const int& type) { amo_type_ = type; }
+    void HandleMoveRighttoLeft();
+
+    bool get_is_move() const { return is_move_; }
+    void set_is_move(bool is_move) { is_move_ = is_move; }
+    void SetWidthHeight(const int& widht, const int& height) { rect_.w = widht; rect_.h = height; };
+    void set_x_val(const int& val) { x_val_ = val; }
+    void set_y_val(const int& val) { y_val_ = val; }
+    int get_x_val() const { return  x_val_; }
+    int get_y_val() const { return  y_val_; }
+private:
+    int x_val_;
+    int y_val_;
+    bool is_move_;
+    int amo_type_;
+};
+
+
+#endif
